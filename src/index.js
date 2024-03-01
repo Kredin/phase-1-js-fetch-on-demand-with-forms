32cd93593e020e9bff33461457da0c55b16bb29c
Blade <bladeprojectz@gmail.com>
2024-03-01 11:30:39 +0800

    lab complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = document.querySelector("input#searchByID");
+    const title = document.querySelector("section#movieDetails h4");
+    const summary = document.querySelector("section#movieDetails p");
 
-document.addEventListener('DOMContentLoaded', init);
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        title.textContent = data.title;
+        summary.textContent = data.summary;
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
